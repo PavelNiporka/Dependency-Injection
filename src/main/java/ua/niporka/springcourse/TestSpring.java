3a8deb14ce70610121e8785ac2e709378b629a26
Niporka <pavelniporka@gmail.com>
2020-10-08 15:41:23 +0300

    Java

--- src/main/java/ua/niporka/springcourse/TestSpring.java
+++ src/main/java/ua/niporka/springcourse/TestSpring.java
@@ -9,28 +9,22 @@ public class TestSpring {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        //Music music = context.getBean("musicBean", Music.class);
-        // MusicPlayer musicPlayer = new MusicPlayer(music);
-
         MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusicList();
+
         boolean comparison = firstMusicPlayer == secondMusicPlayer;
 
         System.out.println(comparison);
         System.out.println(firstMusicPlayer);
         System.out.println(secondMusicPlayer);
-
         firstMusicPlayer.setVolume(10);
         System.out.println(firstMusicPlayer.getVolume());
         System.out.println(secondMusicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
-
-        //System.out.println(musicPlayer.getName());
-        //System.out.println(musicPlayer.getVolume());
 
         context.close();
-
 
     }
 
